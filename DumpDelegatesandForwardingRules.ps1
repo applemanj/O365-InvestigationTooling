529ee3d4cab61aa5e2f97349174658c9f765f01c
applemanj <48220347+applemanj@users.noreply.github.com>
2019-03-04 14:33:23 -0500

    DumpDelegatesandForwardingRules.ps1
    Updated the script to run against only those accounts with mailboxes.

--- DumpDelegatesandForwardingRules.ps1
+++ DumpDelegatesandForwardingRules.ps1
@@ -1,29 +1,25 @@
-﻿#Import the right module to talk with AAD
-import-module MSOnline
-
 #Let's get us an admin cred!
 $userCredential = Get-Credential
-
-#This connects to Azure Active Directory
-Connect-MsolService -Credential $userCredential
 
 $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
 Import-PSSession $ExoSession
 
-$allUsers = @()
-$AllUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | select ObjectID, UserPrincipalName, FirstName, LastName, StrongAuthenticationRequirements, StsRefreshTokensValidFrom, StrongPasswordRequired, LastPasswordChangeTimestamp | Where-Object {($_.UserPrincipalName -notlike "*#EXT#*")}
-
 $UserInboxRules = @()
 $UserDelegates = @()
 
-foreach ($User in $allUsers)
-{
-    Write-Host "Checking inbox rules and delegates for user: " $User.UserPrincipalName;
-    $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectsTo -ne $null)}
-    $UserDelegates += Get-MailboxPermission -Identity $User.UserPrincipalName | Where-Object {($_.IsInherited -ne "True") -and ($_.User -notlike "*SELF*")}
-}
+$mailboxes = Get-Mailbox -ResultSize Unlimited
+$SMTPForwarding  = $mailboxes | select DisplayName,ForwardingAddress,ForwardingSMTPAddress,DeliverToMailboxandForward | where {$_.ForwardingSMTPAddress -ne $null} | ? { $_.ForwardingAddress -ne $null } 
 
-$SMTPForwarding = Get-Mailbox -ResultSize Unlimited | select DisplayName,ForwardingAddress,ForwardingSMTPAddress,DeliverToMailboxandForward | where {$_.ForwardingSMTPAddress -ne $null}
+$i = 1
+foreach ($User in $Mailboxes)
+{
+   [int]$percent = [math]::Round($i / $($mailboxes.count) * 100) 
+   write-progress -activity "Checking Mailboxes" -Status "$i/$($mailboxes.count) complete." -PercentComplete $percent
+   Write-Host "Checking inbox rules and delegates for user: " $User.UserPrincipalName;
+   $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectsTo -ne $null)}
+   $UserDelegates += Get-MailboxPermission -Identity $User.UserPrincipalName | Where-Object {($_.IsInherited -ne "True") -and ($_.User -notlike "*SELF*")}
+   $i++
+}
 
 $UserInboxRules | Export-Csv MailForwardingRulesToExternalDomains.csv
 $UserDelegates | Export-Csv MailboxDelegatePermissions.csv
